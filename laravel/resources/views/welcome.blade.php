68c27955cf1727262f1957ada93202cde4989b54
Nolit <yaboonjapan@gmail.com>
2016-03-07 00:59:29 +0900

    Laravel Project init

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel ver 2</title>
+        <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
