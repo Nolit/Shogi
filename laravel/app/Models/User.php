396e8afe74aee0e4645327cf0b549c5e1d638382
Nolit <yaboonjapan@gmail.com>
2016-04-09 12:57:36 +0900

    Userエンティティsave時にRecord・Statusエンティティもsaveされるようになりました(´・ω・`)ノ

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -8,6 +8,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class User extends Model
 {
     use SoftDeletes;
+
+    private $record;
+    private $status;
+    
+    public function __construct(array $attributes = array()) {
+        parent::__construct($attributes);
+        
+        $this->record = new Record();
+        $this->status = new Status();
+    }
     
     public function record()
     {
@@ -16,5 +26,15 @@ class User extends Model
     public function status()
     {
         return $this->hasOne('App\Models\Status');
+    }
+    
+    public function save(array $options = array()) {
+        parent::save($options);
+        if(!isset($this->record->user_id) && !isset($this->status->user_id)){
+            $this->record->user_id = $this->id;
+            $this->status->user_id = $this->id;
+        }
+        $this->record->save();
+        $this->status->save();
     }
 }
